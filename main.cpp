c9d00bc4ae69c863a9b5af592dac1f7c4cefdd99
Eric Araujo <eric.araujo@calvin.edu>
2025-08-11 12:34:11 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/* main.cpp
+ * Student name:
+ * Date:
+ * Author: Prof. Victor Norman
+ * For: CS 112, Lab 0 at Calvin University.
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    cout << "Welcome to CS112!" << endl;
+    return 0;
+}
